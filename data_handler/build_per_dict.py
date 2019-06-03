1defcb85c4d77dbed61fcf507277b069537073b4
nimashiri <nimashiri2012@gmail.com>
2019-06-03 16:20:51 +0430

    crawler added

--- data_handler/build_per_dict.py
+++ data_handler/build_per_dict.py
@@ -1,3 +1,6 @@
 from data_handler.crawl import Crawler
+import scrapy
 
 crawl_obj = Crawler()
+
+crawl_obj.init_request()
--- data_handler/crawl.py
+++ data_handler/crawl.py
@@ -1,8 +1,17 @@
+import scrapy
 
 
 class Crawler():
     def __init__(self):
         pass
 
-    def getdata(self):
-        return "hi"
+    def init_request(self):
+        urs = 'https://www.safaryabi.com/لغات-و-اصطلاحات-سفر-و-گردشگری/'
+        scrapy.Request(url=urs, callback=self.parser)
+
+    def parser(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('the file is saved!' % filename)
