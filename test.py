8e82ce1ececa21ca461b09cefb949eb25c9ebe84
nimashiri <nimashiri2012@gmail.com>
2019-06-08 22:41:59 +0430

    report

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from data_utils import DataHandler
 from FarsiTokenizer import Tokenizer
 import pandas as pd
+from pandas import *
 from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn.feature_extraction import DictVectorizer
 from classifier import MemoryTagger
@@ -21,24 +22,31 @@ clf = SVC(kernel='linear', C=1)
 ft = FeatureTransformer()
 
 
+
 def main():
     # data = dh_obj.load_farsi_tokens(filename)
     # dh_obj.extract_farsi_tokens(data)
     _data = pd.read_csv('data\\my_ner.csv')
     _data = _data.fillna(method="ffill")
 
-    train_data = _data.iloc[0:9]
+    train_data = _data.iloc[36200:36225]
 
-    mt_obj.fit(train_data['word'], train_data['tag'])
+    tags = train_data["tag"].values.tolist()
 
-    print(train_data['word'])
+    # mt_obj.fit(train_data['word'], tags)
 
-    # ft.fit(train_data, train_data['tag'])
-    # enc = ft.transform(train_data)
-
-    pred = cross_val_predict(Pipeline([("feature_map", FeatureTransformer()),
-                                       ("clf", RandomForestClassifier(n_estimators=20, n_jobs=3))]),
-                             X=train_data, y=train_data['tag'], cv=5)
+    ft.fit(train_data, train_data['tag'])
+    enc = ft.transform(train_data)
+    train_data['word'] = enc
+    # df = DataFrame(train_data)
+    # df.to_csv('train_data.csv', sep=',', encoding='utf-8')
+    train_data = pd.read_csv('train_data.csv')
+    # print(train_data['word'].values.tolist())
+    # print(train_data['tag'].values.tolist())
+    pred = cross_val_predict(estimator=RandomForestClassifier(), X=train_data, y=train_data['tag'], cv=5)
+    # pred = cross_val_predict(Pipeline([("feature_map", FeatureTransformer()),
+    #                                    ("clf", RandomForestClassifier(n_estimators=20, n_jobs=3))]),
+    #                          X=train_data, y=train_data['tag'], cv=5)
 
     report = classification_report(y_pred=pred, y_true=train_data['tag'])
     print(report)
